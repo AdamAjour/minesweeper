a97c3224ed857f64ef2752f6de438def88a4f986
Adam Ajour <adamajour2004@gmail.com>
2024-06-01 04:18:56 +0300

    Added the numbers of bombs and lose condition

--- minesweeper.rb
+++ minesweeper.rb
@@ -3,9 +3,10 @@ require 'securerandom'
 Rows = 9
 Columns = 9
 
-gameover = false
+@gameover = false
 list_of_letters = "ABCDEFGHI"
 @square = ""
+white_space_number = 24
 Letter_map = {
   "A" => 0,
   "B" => 1,
@@ -19,22 +20,45 @@ Letter_map = {
 }
 
 @front_board = Array.new(Rows) { |i| Array.new(Columns) { |i| "_" }}
-@back_board = Array.new(Rows) { |i| Array.new(Columns) { |i| "E" }}
+@back_board = Array.new(Rows) { |i| Array.new(Columns) { |i| 0 }}
 
 def nl()
   print("\n")
 end
 
-def fill_back_board()
+def fill_back_board_bomb()
   bomb_count = 10
   while bomb_count != 0
     row_bomb_index = SecureRandom.random_number(9)
     column_bomb_index = SecureRandom.random_number(9)
-    if @back_board[row_bomb_index.to_i][column_bomb_index.to_i] == "E"
+    if @back_board[row_bomb_index.to_i][column_bomb_index.to_i] == 0
       @back_board[row_bomb_index][column_bomb_index] = "*"
       bomb_count -= 1
     end
 
+  end
+end
+
+def in_board(row , column)
+  if ( (row >= 0 && row <= 8) && ( (column >= 0 && column <= 8) )) 
+    return true
+  end
+  return false
+end
+
+def fill_back_board_numbers()
+  for i in 0..8 do
+    for j in 0..8 do
+      if @back_board[i][j] == "*"
+        for x in i-1..i+1 do
+          for y in j-1..j+1 do
+            if(in_board( x , y ) && !(x == i &&  y == j))
+              @back_board[x][y] = @back_board[x][y].to_i + 1
+            end
+          end
+        end
+      end
+    end
   end
 end
 
@@ -52,7 +76,7 @@ def front_board_display()
     print("#{row_number}")
     row_number += 1
     for columns in 0..Columns do
-      print("\t#{@back_board[rows][columns]}")
+      print("\t#{@front_board[rows][columns]}")
     end
     nl()
     nl()
@@ -82,13 +106,17 @@ def define_format()
 end
 
 def take_input()
+  nl()
   print("Choose a square to select: ")
+  nl()
   @square = gets.chomp.upcase
   input_flag = check_valid_input(@square)
   new_square = try_again_input(input_flag)
   select_flag = select_square(new_square)
   while(!select_flag)
+    nl()
     print("Choose another square. Try again: ")
+    nl()
     @square = gets.chomp.upcase
     input_flag = check_valid_input(@square)
     updated_square = try_again_input(input_flag)
@@ -102,7 +130,7 @@ def try_again_input(flag)
     @square = gets.chomp.upcase
     flag = check_valid_input(@square)
   end
-  print("This is the square value : #{@square}")
+
   test_square = flip_input(@square)
   return test_square
 end
@@ -122,23 +150,26 @@ def flip_input(square)
 end
 
 def select_square(square_coordinates)
-  nl()
-  print("this is the square_coordinates[1].to_i: #{square_coordinates[1].to_i - 1}")
-  nl()
   letter_num = Letter_map[square_coordinates[0]]
-  nl()
-  print("this is the letter_num: #{letter_num}")
-  nl()
   if  @front_board[(square_coordinates[1].to_i - 1)][letter_num] != @back_board[(square_coordinates[1].to_i - 1)][letter_num]
     @front_board[(square_coordinates[1].to_i - 1)][letter_num] = @back_board[(square_coordinates[1].to_i - 1)][letter_num]
+    if @back_board[(square_coordinates[1].to_i - 1)][letter_num] == "*"
+      board_boarders()
+      front_board_display()
+      nl()
+      print("Game Over!")
+      nl()
+      @gameover = true
+    end
     return true
   end
   return false
 end
 
-fill_back_board()
+fill_back_board_bomb()
+fill_back_board_numbers()
 define_format()
-while(!gameover)
+while(!@gameover)
   board_boarders()
   front_board_display()
   take_input()
